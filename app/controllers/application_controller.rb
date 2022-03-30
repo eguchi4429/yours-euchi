a10baffb80ba6250a088fddae2cd00cca577016b
eguchi <wellstone2@gmail.com>
2022-03-30 13:09:41 +0900

    画面開発前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
+  protect_from_forgery #CSRF対策
 
   # ここから追加します
   before_action :configure_permitted_parameters, if: :devise_controller?
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'yours#index'
+  get 'yours/index' # 自動で設定されたルーティング
   namespace :v1 do
     mount_devise_token_auth_for "User", at: "auth"
   end
